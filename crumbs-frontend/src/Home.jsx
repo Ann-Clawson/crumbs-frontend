812ea110c7fdfba201f89e031c876ef00d7a4b58
Ann Clawson <annmasonclawson@gmail.com>
2024-08-16 10:16:44 -0700

    adds text to home

--- crumbs-frontend/src/Home.jsx
+++ crumbs-frontend/src/Home.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 export function Home() {
   return (
     <div>
       <img src="./assets/CaramelChocChip_sash-1-1.jpg" alt="Caramel Chocolate Chip Cookie" />
       <img src="./assets/trefoil.jpeg" />
-      <h1>Crumbs<h1/>
+      <h1>Crumbs</h1>
     </div>
   );
 }
