8a640d3c5212952f1f9c0013f3cb0ada45a33409
Ann Clawson <annmasonclawson@gmail.com>
2024-08-16 10:27:09 -0700

    adds simple styling to home

--- crumbs-frontend/src/Home.jsx
+++ crumbs-frontend/src/Home.jsx
@@ -2,8 +2,8 @@ import caramelChocChip from "./assets/CaramelChocChip_sash-1-1.jpg";
 
 export function Home() {
   return (
-    <div>
-      <img src={caramelChocChip} alt="Caramel Chocolate Chip Cookie" />
+    <div className="home-page">
+      <img src={caramelChocChip} width="300" alt="Caramel Chocolate Chip Cookie" />
       <h1>Crumbs</h1>
     </div>
   );
