a53dcf6d51d5295010499eed146cbd178530c03a
Ann Clawson <annmasonclawson@gmail.com>
2024-08-16 10:45:45 -0700

    adds styling to home

--- crumbs-frontend/src/Home.jsx
+++ crumbs-frontend/src/Home.jsx
@@ -4,7 +4,9 @@ export function Home() {
   return (
     <div className="home-page">
       <img src={caramelChocChip} width="300" alt="Caramel Chocolate Chip Cookie" />
-      <h1>Crumbs</h1>
+      <h1 className="home-crumbs">
+        Crumbs<span className="copyright"> &copy;</span>
+      </h1>
     </div>
   );
 }
