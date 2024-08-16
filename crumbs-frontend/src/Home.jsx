ffe560dc90b8d60a2e4712d1071b55c035aa2519
Ann Clawson <annmasonclawson@gmail.com>
2024-08-16 10:14:58 -0700

    .

--- crumbs-frontend/src/Home.jsx
+++ crumbs-frontend/src/Home.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 export function Home() {
   return (
     <div>
       <img src="./assets/CaramelChocChip_sash-1-1.jpg" alt="Caramel Chocolate Chip Cookie" />
       <img src="./assets/trefoil.jpeg" />
-      {/* <h1>Crumbs<h1/> */}
+      <h1>Crumbs<h1/>
     </div>
   );
 }
