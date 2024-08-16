a3a2568c72fd1e0f23198e4d5d71473692b2e4e0
Ann Clawson <annmasonclawson@gmail.com>
2024-08-16 10:19:29 -0700

    adds image to home

--- crumbs-frontend/src/Home.jsx
+++ crumbs-frontend/src/Home.jsx
@@ -1,8 +1,9 @@
+import caramelChocChip from "./assets/CaramelChocChip_sash-1-1.jpg";
+
 export function Home() {
   return (
     <div>
-      <img src="./assets/CaramelChocChip_sash-1-1.jpg" alt="Caramel Chocolate Chip Cookie" />
-      <img src="./assets/trefoil.jpeg" />
+      <img src={caramelChocChip} alt="Caramel Chocolate Chip Cookie" />
       <h1>Crumbs</h1>
     </div>
   );
