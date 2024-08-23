9e98824bb5dec39ba27a52e6c1430ab4a7ff5fc1
Ann Clawson <annmasonclawson@gmail.com>
2024-08-23 06:02:10 -0700

    adds logic to get user and print to the console

--- crumbs-frontend/src/Content.jsx
+++ crumbs-frontend/src/Content.jsx
@@ -1,6 +1,18 @@
 import { Home } from "./Home";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export function Content() {
+  const [user, setUser] = useState({});
+  const getUser = (userID) => {
+    axios.get(`http://localhost:5000/users/${userID}.json`).then((response) => {
+      setUser(response.data);
+    });
+  };
+
+  useEffect(getUser, []);
+  console.log(user);
+
   return (
     <div>
       <Home />
